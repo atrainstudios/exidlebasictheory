c1d773bd438b3ec7b2e7ff5e00cdcf09fe1b0bf4
atrainstudios <98567098+atrainstudios@users.noreply.github.com>
2022-02-10 15:34:06 -0500

    Update CustomTheory.js

--- CustomTheory.js
+++ CustomTheory.js
@@ -7,7 +7,7 @@ import { Utils } from "./api/Utils";
 var id = "ouo";
 var name = "Basic Theory";
 var description = "This theory leads to a beautiful conclusion and is based off of the basic starter theory you get when making custom theories. It has a LOT of story chapters but you'll be satisfied in the end :) ouo.";
-var authors = "invalid-user#2123";
+var authors = "invalid-user#7960";
 var version = 2.1;
 
 var currency;
